c9e1a4a02420c98d3c698dd5d99cbd8100e5eb4e
Andres Molestina <zocrates@gmail.com>
2021-09-23 13:50:06 -0400

    REACT portfolio, still need more projects to be added

---
+++ src/components/ContactUs.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+export default class ContactUs extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="contact">
+          <div className="row section-head">
+            <div className="ten columns">
+              <p className="lead">
+              Contact me for any work or suggestions
+              </p>
+            </div>
+          </div>
+          <div className="row">
+            <aside className="eigth columns footer-widgets">
+              <div className="widget">
+                <h4>Linked in :
+                  <div />{resumeData.linkedinId}
+                  <div />{resumeData.email}
+                  <div />{resumeData.phone}
+                </h4>
+              </div>
+            </aside>
+          </div>
+        </section>
+        );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,118 @@
+let resumeData = {
+    "imagebaseurl":"placeholder",
+    "name": "Andres Molestina",
+    "role": "Full Stack Developer, Father, Veteran",
+    "linkedinId":"@andres-molestina",
+    "skypeid": "@zocrates",
+    "email":"zocrates@gmail.com",
+    "phone":"704-430-5677",
+    "roleDescription": "I have built a solid foundation at UNCC BCS and am ready to use my knowledge to both advance my carrer and grow my knowledge.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/andres-molestina-411b8237/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/zocartes",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"twitter",
+          "url":"http://twitter.com/worldtrecks",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"Just graduated BCS @ UNCC.  I am looking to bring my expirience as both a Veteran and an Entrepreneur to bear in the field of Coding.  My unique insights into Management and leadership should prove useful in developing tools for said fields. ",
+    "address":"162 Valleyview Rd. Mooresville NC 28117",
+    "website":"https://github.com/Zocartes",
+    "education":[
+      {
+        "UniversityName":"Boot Camp Spot @ UNCC",
+        "specialization":"Full Stack Web Developer",
+        "MonthOfPassing":"October",
+        "YearOfPassing":"2021",
+        "Achievements":"Graduated"
+      },
+      {
+        "UniversityName":"USArmy Corps of Engineers",
+        "specialization":"52D Power Mobilization specialist",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2000",
+        "Achievements":"Awarded ARCOM medal for top grad"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"MeiLi Beauty",
+        "specialization":"Owner-Manager",
+        "MonthOfLeaving":"Currently",
+        "YearOfLeaving":"working",
+        "Achievements":"Designed, built, staffed, run and manage a 12 chair Salon"
+      },
+      {
+        "CompanyName":"Nail Art Beauty Spa",
+        "specialization":"Owner-Manager",
+        "MonthOfLeaving":"Feb",
+        "YearOfLeaving":"2016",
+        "Achievements":"Designed, built, staffed and managed"
+      }
+    ],
+    "skillsDescription":"Technologies",
+    "skills":[
+      {
+        "skillname":"NODE"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Bootstrap"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"Redux"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Sceduler",
+        "description":"Track your schedule trough the day",
+        "imgurl":"https://github.com/Zocartes/day-planner-homework/blob/main/assets/pics/daily-planner.png?raw=true",
+        "href":"https://zocartes.github.io/day-planner-homework/"
+      },
+      {
+        "name":"NODE Company Management",
+        "description":"command line app to manage company staff and salaries",
+        "imgurl":"https://i.imgur.com/X2xfISZ.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
